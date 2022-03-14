cb229258c24bc0c69a2fdc0de3c2444bb1b173c3
Hong-Kit Randy Yeung <hong-kit.r.yeung@jpl.nasa.gov>
2022-03-14 14:20:40 -0500

    clearing out develop branch

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-poetry build
-poetry run pip install --upgrade -t package dist/*.whl
-cd package ; zip -r ../artifact.zip . -x '*.pyc'
-cd ..
-code_version=`python -c 'from cumulus_publish_cnm import __version__; print(__version__)'`
-mv artifact.zip artifact-$code_version.zip
