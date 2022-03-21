a18784e0c5e9587ce3d6aaab88096ffbe1ee746a
Hong-Kit Randy Yeung <hong-kit.r.yeung@jpl.nasa.gov>
2022-03-21 17:48:20 -0500

    fix build script

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ poetry build
 poetry run pip install --upgrade -t package dist/*.whl
 cd package ; zip -r ../artifact.zip . -x '*.pyc'
 cd ..
-code_version=`python -c 'from cumulus_publish_cnm import __version__; print(__version__)'`
-mv artifact.zip artifact-$code_version.zip
+code_version=`poetry version | awk '{print $2}'`
+cp artifact.zip artifact-$code_version.zip
+echo \*\* artifact-$code_version.zip created \*\*
