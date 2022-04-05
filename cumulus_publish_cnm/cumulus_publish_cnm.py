77a63a6e6455b498414cd833a68bd575004c1f69
Hong-Kit Randy Yeung <hong-kit.r.yeung@jpl.nasa.gov>
2022-04-05 13:25:31 -0500

    fix indentation issues

--- cumulus_publish_cnm/cumulus_publish_cnm.py
+++ cumulus_publish_cnm/cumulus_publish_cnm.py
@@ -7,40 +7,40 @@ logger = CumulusLogger('publish_cnm_logger')
 
 
 class PublishCNM(Process):
-	className = 'PublishCNM'
+    className = 'PublishCNM'
 
-	def __init__(self, *args, **kwargs):
-		super(PublishCNM, self).__init__(*args, **kwargs)
-		self.logger = logger
-		self.logger.debug('{} Entered __init__', self.className)
+    def __init__(self, *args, **kwargs):
+        super(PublishCNM, self).__init__(*args, **kwargs)
+        self.logger = logger
+        self.logger.debug('{} Entered __init__', self.className)
 
-		required = ['sns_endpoint']
+        required = ['sns_endpoint']
 
-		for requirement in required:
-			if requirement not in self.config.keys():
-				raise Exception('%s config key is missing' % requirement)
+        for requirement in required:
+            if requirement not in self.config.keys():
+                raise Exception('%s config key is missing' % requirement)
 
-	def process(self):
-		# config
-		meta_sns_endpoint = self.config.get('sns_endpoint', '')
+    def process(self):
+        # config
+        meta_sns_endpoint = self.config.get('sns_endpoint', '')
 
-		self.logger.debug('sns_endpoint: {}', meta_sns_endpoint)
+        self.logger.debug('sns_endpoint: {}', meta_sns_endpoint)
 
-		sns = boto3.resource('sns')
-		# Trigger SNS into ingestion stream (for each granule)
-		platform_endpoint = sns.PlatformEndpoint(meta_sns_endpoint)
-		response = []
+        sns = boto3.resource('sns')
+        # Trigger SNS into ingestion stream (for each granule)
+        platform_endpoint = sns.PlatformEndpoint(meta_sns_endpoint)
+        response = []
 
-		for item in self.input['cnm_list']:
-			r = platform_endpoint.publish(
-				Message=json.dumps(item)
-			)
-			response.append(r)
-		logger.debug(json.dumps(self.input))
+        for item in self.input['cnm_list']:
+            r = platform_endpoint.publish(
+                Message=json.dumps(item)
+            )
+            response.append(r)
+        logger.debug(json.dumps(self.input))
 
-		return response
+        return response
 
 
 def lambda_handler(event, context):
-	logger.setMetadata(event, context)
-	return PublishCNM.cumulus_handler(event, context=context)
+    logger.setMetadata(event, context)
+    return PublishCNM.cumulus_handler(event, context=context)
